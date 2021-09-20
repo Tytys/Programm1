35c0fc1961120f119b5255ec0502b7c6cd71029b
79858 <79858@DESKTOP-C4A7714>
2021-09-20 11:53:29 +0300

    Добавьте файлы проекта.

---
+++ Programm1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programm1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 3.14, y = 17.25;
+
+            double add, sub, mul, div;
+
+            add = x + y;
+
+            sub = x - y;
+
+            mul = x * y;
+
+            div = x / y;
+
+            Console.Write("add = " + add + "\n\n" +
+                           "sub = " + sub + "\n\n" +
+                           "mul = " + mul + "\n\n" +
+                           "div = " + div);
+
+            Console.ReadLine();
+        }
+    }
+}
